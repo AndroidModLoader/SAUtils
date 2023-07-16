a9e6f87d7f0893cf8b9c9587175003144cfa85c5
RusJJ <mctesting1@yandex.ru>
2023-07-16 20:58:07 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,11 +2,12 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cpp .cc
-LOCAL_MODULE    := SAUtils
 LOCAL_SRC_FILES := main.cpp sa_scripting.cpp mod/logger.cpp
 ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
+    LOCAL_MODULE    := SAUtils
     LOCAL_SRC_FILES += sautils.cpp sautils_2_10.cpp
 else
+    LOCAL_MODULE    := SAUtils64
     LOCAL_SRC_FILES += sautils_2_10_x64.cpp
 endif
 LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
